c5e8140f9e5855ee06f4dee83ec4c63f42ba0ebe
Toshiki Ikda <b1013001@fun.ac.jp>
2018-05-12 16:51:22 +0900

    画面追加

--- manji/Views/DetailViewController.swift
+++ manji/Views/DetailViewController.swift
@@ -1,34 +1,45 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  manji
 //
-//  Created by 会津慎弥 on 2018/05/12.
+//  Created by 池田俊輝 on 2018/05/12.
 //  Copyright © 2018年 会津慎弥. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func detail(_ sender: Any) {
+    
+    
+    @IBAction func back(_ sender: Any) {
         //まずは、同じstororyboard内であることをここで定義します
         let storyboard: UIStoryboard = self.storyboard!
         //ここで移動先のstoryboardを選択(今回の場合は先ほどsecondと名付けたのでそれを書きます)
-        let second = storyboard.instantiateViewController(withIdentifier: "detail")
+        let second = storyboard.instantiateViewController(withIdentifier: "home")
         //ここが実際に移動するコードとなります
         self.present(second, animated: true, completion: nil)
     }
     
-}
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
