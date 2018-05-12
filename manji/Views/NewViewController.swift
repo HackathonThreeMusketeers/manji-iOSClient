c0ae1d31b496530ab7a671c98806d406b8b00451
Toshiki Ikda <b1013001@fun.ac.jp>
2018-05-13 08:16:37 +0900

    デザイン修正

--- manji/Views/NewViewController.swift
+++ manji/Views/NewViewController.swift
@@ -11,12 +11,71 @@ import UIKit
 class NewViewController: UIViewController {
     
     let imageBag = UIImageView()
-    var labelX = 0
-    var labelY = 0
+
+    
+    var imy = 128
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+    // Screen Size の取得
+    let screenWidth:CGFloat = view.frame.size.width
+    let screenHeight:CGFloat = view.frame.size.height
+    // ハンドバッグの画像を設定
+    imageBag.image = UIImage(named: "img_07.png")
+    // 画像のフレームを設定
+    imageBag.frame = CGRect(x:0, y:0, width:128, height:imy)
+    
+    // 画像をスクリーン中央に設定
+    imageBag.center = CGPoint(x:screenWidth/2, y:600)
+    
+    // タッチ操作を enable
+    imageBag.isUserInteractionEnabled = true
+    
+    self.view.addSubview(imageBag)
+    
 
-        // Do any additional setup after loading the view.
+    // 画面背景を設定
+    self.view.backgroundColor = UIColor(red:0.85,green:1.0,blue:0.95,alpha:1.0)
+        
+    // Do any additional setup after loading the view.
+    }
+    
+    // 画面にタッチで呼ばれる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touchesBegan")
+        
+    }
+    
+    //　ドラッグ時に呼ばれる
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // タッチイベントを取得
+        let touchEvent = touches.first!
+        
+        // ドラッグ前の座標, Swift 1.2 から
+        let preDy = touchEvent.previousLocation(in: self.view).y
+        
+        // ドラッグ後の座標
+        var newDy = touchEvent.location(in: self.view).y
+        
+        
+        // ドラッグしたy座標の移動距離
+        let dy = newDy - preDy
+        
+        // 画像のフレーム
+        var viewFrame: CGRect = imageBag.frame
+        
+        // 移動分を反映させる
+        viewFrame.origin.y += dy
+        
+        imageBag.frame = viewFrame
+        
+        self.view.addSubview(imageBag)
+        
+        if(newDy<300){
+            imageBag.frame = CGRect(x:0, y:100000, width:128, height:imy)
+        }
+        print(newDy)
     }
 
     override func didReceiveMemoryWarning() {
