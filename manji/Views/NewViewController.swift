f9170a6e0a41bdcf6fcc7fd8781af19a87c890fa
Toshiki Ikda <b1013001@fun.ac.jp>
2018-05-12 17:29:39 +0900

    ３画面に追加

---
+++ manji/Views/NewViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NewViewController.swift
+//  manji
+//
+//  Created by 池田俊輝 on 2018/05/12.
+//  Copyright © 2018年 会津慎弥. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func back(_ sender: Any) {
+        //まずは、同じstororyboard内であることをここで定義します
+        let storyboard: UIStoryboard = self.storyboard!
+        //ここで移動先のstoryboardを選択(今回の場合は先ほどsecondと名付けたのでそれを書きます)
+        let second = storyboard.instantiateViewController(withIdentifier: "home")
+        //ここが実際に移動するコードとなります
+        self.present(second, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
