bc5e0c7f6987afb10c3f69e7da9ddaa269d9f0d7
hiriorima <g2117001@fun.ac.jp>
2018-05-12 15:22:35 +0900

    Initial Commit

---
+++ manji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  manji
+//
+//  Created by 会津慎弥 on 2018/05/12.
+//  Copyright © 2018年 会津慎弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
